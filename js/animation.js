c1da7710d6c398d5a3b708de1f462c9a84ae41e0
Imelda <adlewic@gmail.com>
2016-09-29 11:20:43 -0500

    tranform rotation mouse

--- js/animation.js
+++ js/animation.js
@@ -1,6 +1,20 @@
 $( document ).ready(function() {
+	
+
+
+	$("p")
+	  .velocity("fadeIn", { delay: 100, duration: 1000 });
+
+
 	$(".st0")
-	  .velocity("fadeOut", { delay: 500, duration: 2000 });
+	  .velocity("fadeOut", { delay: 2000, duration: 2000 });
+
+	$(".earphone")
+	  .velocity("fadeOut", { delay: 2000, duration: 2000 });
+
+	 $("p")
+		.velocity("fadeOut",{ delay: 2000, duration: 2000});
+
 });
 
 
